a76e13113263ba72253f9f16914dbcc2a454ee9f
Yash Chauhan <yashc9411407582@gmail.com>
2025-01-28 22:38:40 +0530

    Implement REST API for To-Do application with CRUD operations and update front-end to interact with the API

--- app.py
+++ app.py
@@ -75,3 +75,77 @@ def toggleStatus(id):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+"""
+REST API approach for getting the data; 
+
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+api = Api(app)
+
+# Task Model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f"Task(id: {self.id}, task: {self.task}, status: {self.status})"
+
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
+
+
+# REST API Endpoints
+class TodoList(Resource):
+    def get(self):
+        todos = Todo.query.all()
+        return jsonify([{'id': todo.id, 'task': todo.task, 'status': todo.status} for todo in todos])
+
+    def post(self):
+        data = request.get_json()
+        new_task = Todo(task=data['task'])
+        db.session.add(new_task)
+        db.session.commit()
+        return jsonify({'id': new_task.id, 'task': new_task.task, 'status': new_task.status})
+
+class TodoItem(Resource):
+    def get(self, id):
+        task = Todo.query.get_or_404(id)
+        return jsonify({'id': task.id, 'task': task.task, 'status': task.status})
+
+    def put(self, id):
+        data = request.get_json()
+        task = Todo.query.get_or_404(id)
+        task.task = data['task']
+        task.status = data.get('status', task.status)
+        db.session.commit()
+        return jsonify({'id': task.id, 'task': task.task, 'status': task.status})
+
+    def delete(self, id):
+        task = Todo.query.get_or_404(id)
+        db.session.delete(task)
+        db.session.commit()
+        return '', 204
+
+
+# Add API resources
+api.add_resource(TodoList, '/api/todos')
+api.add_resource(TodoItem, '/api/todos/<int:id>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+"""
